de235e9fa4b7d41185d9256b592a3e8569b6fe64
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-10-18 08:30:24 +1100

    Begin handling cart functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -70,18 +70,27 @@ const formatter = new Intl.NumberFormat('en-US', {
 const App: React.FC = () => {
   const [cartItems, setCartItems] = useState<any[]>([]);
 
+  const handleAddRemove = (id: number, newQty: number) => {
+    console.log('handleAddRemove', id, newQty);
+  };
+
   return (
     <section className="container">
-      <Menu menuItems={menuItems} cartItems={cartItems} />
+      <Menu
+        menuItems={menuItems}
+        cartItems={cartItems}
+        handleAddRemove={handleAddRemove}
+      />
       <Cart cartItems={cartItems} />
     </section>
   );
 };
 
-const Menu: React.FC<{ menuItems: object[]; cartItems: any[] }> = ({
-  menuItems,
-  cartItems
-}) => {
+const Menu: React.FC<{
+  menuItems: object[];
+  cartItems: any[];
+  handleAddRemove: Function;
+}> = ({ menuItems, cartItems, handleAddRemove }) => {
   return (
     <div id="menu" className="panel panel__menu">
       <h2>To Go Menu</h2>
@@ -91,7 +100,13 @@ const Menu: React.FC<{ menuItems: object[]; cartItems: any[] }> = ({
             const isInCart: boolean = !!cartItems.find(
               (cartItem) => cartItem.id === item.id
             );
-            return <MenuItem item={item} isInCart={isInCart} />;
+            return (
+              <MenuItem
+                item={item}
+                isInCart={isInCart}
+                handleAddRemove={handleAddRemove}
+              />
+            );
           })}
         </ul>
       </div>
@@ -99,10 +114,13 @@ const Menu: React.FC<{ menuItems: object[]; cartItems: any[] }> = ({
   );
 };
 
-const MenuItem: React.FC<{ item: any; isInCart: boolean }> = ({
-  item,
-  isInCart
-}) => {
+const MenuItem: React.FC<{
+  item: any;
+  isInCart: boolean;
+  handleAddRemove: any;
+}> = ({ item, isInCart, handleAddRemove }) => {
+  const newQty = !isInCart ? 1 : 0;
+
   return (
     <li className="menu__item">
       <h3 className="item__description">{item.description}</h3>
@@ -112,6 +130,7 @@ const MenuItem: React.FC<{ item: any; isInCart: boolean }> = ({
       </div>
       <button
         className={`item__add-remove${isInCart ? ' button__active' : ''}`}
+        onClick={() => handleAddRemove(item.id, newQty)}
       >
         <span className="button__content">
           {isInCart ? 'In Cart' : 'Add to Cart'}
@@ -125,74 +144,80 @@ const Cart: React.FC<{ cartItems: object[] }> = ({ cartItems }) => {
   return (
     <div id="cart" className="panel panel__cart">
       <h2>Your Cart</h2>
-      {cartItems.length === 0 && <p>Your cart is empty</p>}
-      <div className="panel__scroll-area">
-        <ul id="cart__items">
-          <li className="cart__item">
-            <h3 className="item__description">Hot Chips with Tomato Sauce</h3>
-            <div className="item__image-container">
-              <img
-                src="./assets/images/plate__french-fries.png"
-                alt="hot chips with tomato sauce"
-              />
-              <div className="image__count-badge">2</div>
-            </div>
-            <div className="price">$2.23</div>
-            <div className="item__line">
-              <div className="line__options">
-                <button className="options__decrement"></button>
-                <span className="options__qty">2</span>
-                <button className="options__increment"></button>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div className="panel__scroll-area">
+          <ul id="cart__items">
+            {cartItems.map((cartItem) => (
+              <CartItem cartItem={cartItem} />
+            ))}
+            <li className="cart__item">
+              <h3 className="item__description">Hot Chips with Tomato Sauce</h3>
+              <div className="item__image-container">
+                <img
+                  src="./assets/images/plate__french-fries.png"
+                  alt="hot chips with tomato sauce"
+                />
+                <div className="image__count-badge">2</div>
               </div>
-              <span className="price price__large">$4.46</span>
-            </div>
-          </li>
-          <li className="cart__item">
-            <h3 className="item__description">Spaghetti with Sauce</h3>
-            <div className="item__image-container">
-              <img
-                src="./assets/images/plate__spaghetti-meat-sauce.png"
-                alt="spaghetti with sauce"
-              />
-              <div className="image__count-badge">2</div>
-            </div>
-            <div className="price">$7.82</div>
-            <div className="item__line">
-              <div className="line__options">
-                <button className="options__decrement"></button>
-                <span className="options__qty">2</span>
-                <button className="options__increment"></button>
+              <div className="price">$2.23</div>
+              <div className="item__line">
+                <div className="line__options">
+                  <button className="options__decrement"></button>
+                  <span className="options__qty">2</span>
+                  <button className="options__increment"></button>
+                </div>
+                <span className="price price__large">$4.46</span>
               </div>
-              <span className="price price__large">$15.64</span>
-            </div>
-          </li>
-        </ul>
-        <hr />
-        <div className="cart__totals">
-          <div className="totals__label">Subtotal:</div>
-          <div className="price price__large">$20.10</div>
-          <div className="totals__label">Tax:</div>
-          <div className="price price__large">$1.96</div>
-          <div className="totals__label">Total:</div>
-          <div className="price price__large price__total">$30.12</div>
+            </li>
+            <li className="cart__item">
+              <h3 className="item__description">Spaghetti with Sauce</h3>
+              <div className="item__image-container">
+                <img
+                  src="./assets/images/plate__spaghetti-meat-sauce.png"
+                  alt="spaghetti with sauce"
+                />
+                <div className="image__count-badge">2</div>
+              </div>
+              <div className="price">$7.82</div>
+              <div className="item__line">
+                <div className="line__options">
+                  <button className="options__decrement"></button>
+                  <span className="options__qty">2</span>
+                  <button className="options__increment"></button>
+                </div>
+                <span className="price price__large">$15.64</span>
+              </div>
+            </li>
+          </ul>
+          <hr />
+          <div className="cart__totals">
+            <div className="totals__label">Subtotal:</div>
+            <div className="price price__large">$20.10</div>
+            <div className="totals__label">Tax:</div>
+            <div className="price price__large">$1.96</div>
+            <div className="totals__label">Total:</div>
+            <div className="price price__large price__total">$30.12</div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
 
-const CartItem: React.FC<{ item: object }> = ({ item }) => {
+const CartItem: React.FC<{ cartItem: any }> = ({ cartItem }) => {
   return (
     <li className="cart__item">
-      <h3 className="item__description">Hot Chips with Tomato Sauce</h3>
+      <h3 className="item__description">{cartItem.description}</h3>
       <div className="item__image-container">
         <img
           src="./assets/images/plate__french-fries.png"
-          alt="hot chips with tomato sauce"
+          alt={cartItem.description?.toLowerCase()}
         />
         <div className="image__count-badge">2</div>
       </div>
-      <div className="price">$2.23</div>
+      <div className="price">{formatter.format(cartItem.price / 100)}</div>
       <div className="item__line">
         <div className="line__options">
           <button className="options__decrement"></button>
